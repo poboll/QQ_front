8751a01ea15dadd5a916d033c72999b7d7e795f7
在虎 <caiths@icloud.com>
2023-11-19 22:14:24 +0800

    Update FriendsCom.vue

--- src/components/FriendsCom.vue
+++ src/components/FriendsCom.vue
@@ -127,59 +127,59 @@ export default {
             myFriends: { //好友对象，存储所有好友的信息
                 lafei: {
                     path: '/card',
-                    query: { profile:'profile_拉菲.jpg', name:'拉菲', qq:'1164585301', gender:'女', age:15, birth_month:10, birth_day:30, constellation:'天蝎', place:'日本东京', sign:'睡觉觉~' }
+                    query: { profile:'profile_拉菲.jpg', name:'拉菲', qq:'83000001', gender:'女', age:15, birth_month:10, birth_day:30, constellation:'天蝎', place:'日本东京', sign:'睡觉觉~' }
                 },
                 zerotwo: {
                     path: '/card',
-                    query: { profile:'profile_02.jpg', name:'02', qq:'1164585302', gender:'女', age:18, birth_month:2, birth_day:27, constellation:'双鱼', place:'日本东京', sign:'darling~' }
+                    query: { profile:'profile_02.jpg', name:'02', qq:'83000002', gender:'女', age:18, birth_month:2, birth_day:27, constellation:'双鱼', place:'日本东京', sign:'darling~' }
                 },
                 mirai: {
                     path: '/card',
-                    query: { profile:'profile_栗山未来.jpg', name:'栗山未来', qq:'1164585303', gender:'女', age:17, birth_month:3, birth_day:31, constellation:'白羊', place:'日本东京', sign:'不愉快~' }
+                    query: { profile:'profile_栗山未来.jpg', name:'栗山未来', qq:'83000003', gender:'女', age:17, birth_month:3, birth_day:31, constellation:'白羊', place:'日本东京', sign:'不愉快~' }
                 },
                 zhezhi: {
                     path: '/card',
-                    query: { profile:'profile_鸢一折纸.jpg', name:'鸢一折纸', qq:'1164585304', gender:'女', age:16, birth_month:11, birth_day:11, constellation:'天蝎', place:'日本东京', sign:'绝灭天使' }
+                    query: { profile:'profile_鸢一折纸.jpg', name:'鸢一折纸', qq:'83000004', gender:'女', age:16, birth_month:11, birth_day:11, constellation:'天蝎', place:'日本东京', sign:'绝灭天使' }
                 },
                 erya: {
                     path: '/card',
-                    query: { profile:'profile_本条二亚.jpg', name:'本条二亚', qq:'1164585305', gender:'女', age:17, birth_month:2, birth_day:29, constellation:'双鱼', place:'日本东京', sign:'嗫告篇帙' }
+                    query: { profile:'profile_本条二亚.jpg', name:'本条二亚', qq:'83000005', gender:'女', age:17, birth_month:2, birth_day:29, constellation:'双鱼', place:'日本东京', sign:'嗫告篇帙' }
                 },
                 kuangsan: {
                     path: '/card',
-                    query: { profile:'profile_时崎狂三.jpg', name:'时崎狂三', qq:'1164585306', gender:'女', age:17, birth_month:6, birth_day:10, constellation:'双子', place:'日本东京', sign:'刻刻帝' }
+                    query: { profile:'profile_时崎狂三.jpg', name:'时崎狂三', qq:'83000006', gender:'女', age:17, birth_month:6, birth_day:10, constellation:'双子', place:'日本东京', sign:'刻刻帝' }
                 },
                 siminai: {
                     path: '/card',
-                    query: { profile:'profile_四糸乃.jpg', name:'四糸乃', qq:'1164585307', gender:'女', age:13, birth_month:3, birth_day:20, constellation:'双鱼', place:'日本东京', sign:'冰结傀儡' }
+                    query: { profile:'profile_四糸乃.jpg', name:'四糸乃', qq:'83000007', gender:'女', age:13, birth_month:3, birth_day:20, constellation:'双鱼', place:'日本东京', sign:'冰结傀儡' }
                 },
                 qinli: {
                     path: '/card',
-                    query: { profile:'profile_五河琴里.jpg', name:'五河琴里', qq:'1164585308', gender:'女', age:14, birth_month:8, birth_day:3, constellation:'狮子', place:'日本东京', sign:'灼烂歼鬼' }
+                    query: { profile:'profile_五河琴里.jpg', name:'五河琴里', qq:'83000008', gender:'女', age:14, birth_month:8, birth_day:3, constellation:'狮子', place:'日本东京', sign:'灼烂歼鬼' }
                 },
                 liucan: {
                     path: '/card',
-                    query: { profile:'profile_星宫六喰.jpg', name:'星宫六喰', qq:'1164585309', gender:'女', age:14, birth_month:9, birth_day:12, constellation:'处女', place:'日本东京', sign:'封解主' }
+                    query: { profile:'profile_星宫六喰.jpg', name:'星宫六喰', qq:'83000009', gender:'女', age:14, birth_month:9, birth_day:12, constellation:'处女', place:'日本东京', sign:'封解主' }
                 },
                 qizui: {
                     path: '/card',
-                    query: { profile:'profile_镜野七罪.jpg', name:'镜野七罪', qq:'1164585310', gender:'女', age:13, birth_month:7, birth_day:23, constellation:'狮子', place:'日本东京', sign:'赝造魔女' }
+                    query: { profile:'profile_镜野七罪.jpg', name:'镜野七罪', qq:'83000010', gender:'女', age:13, birth_month:7, birth_day:23, constellation:'狮子', place:'日本东京', sign:'赝造魔女' }
                 },
                 xixuan: {
                     path: '/card',
-                    query: { profile:'profile_八舞夕弦.jpg', name:'八舞夕弦', qq:'1164585311', gender:'女', age:18, birth_month:10, birth_day:18, constellation:'天秤', place:'日本东京', sign:'飓风骑士' }
+                    query: { profile:'profile_八舞夕弦.jpg', name:'八舞夕弦', qq:'83000011', gender:'女', age:18, birth_month:10, birth_day:18, constellation:'天秤', place:'日本东京', sign:'飓风骑士' }
                 },
                 yejushi: {
                     path: '/card',
-                    query: { profile:'profile_八舞耶俱矢.jpg', name:'八舞耶俱矢', qq:'1164585312', gender:'女', age:18, birth_month:10, birth_day:18, constellation:'天秤', place:'日本东京', sign:'飓风骑士' }
+                    query: { profile:'profile_八舞耶俱矢.jpg', name:'八舞耶俱矢', qq:'83000012', gender:'女', age:18, birth_month:10, birth_day:18, constellation:'天秤', place:'日本东京', sign:'飓风骑士' }
                 },
                 meijiu: {
                     path: '/card',
-                    query: { profile:'profile_诱宵美九.jpg', name:'诱宵美九', qq:'1164585313', gender:'女', age:18, birth_month:1, birth_day:19, constellation:'摩羯', place:'日本东京', sign:'破军歌姬' }
+                    query: { profile:'profile_诱宵美九.jpg', name:'诱宵美九', qq:'83000013', gender:'女', age:18, birth_month:1, birth_day:19, constellation:'摩羯', place:'日本东京', sign:'破军歌姬' }
                 },
                 shixiang: {
                     path: '/card',
-                    query: { profile:'profile_夜刀神十香.jpg', name:'夜刀神十香', qq:'1164585314', gender:'女', age:18, birth_month:4, birth_day:10, constellation:'白羊', place:'日本东京', sign:'鏖杀公' }
+                    query: { profile:'profile_夜刀神十香.jpg', name:'夜刀神十香', qq:'83000014', gender:'女', age:18, birth_month:4, birth_day:10, constellation:'白羊', place:'日本东京', sign:'鏖杀公' }
                 }
             }
         };
